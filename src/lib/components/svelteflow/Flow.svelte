abf22abc6935161ec092b11f52a296ae5a2f5ba0
Nacer Berkane <nacer.ayed@outlook.fr>
2024-11-17 13:11:32 +0100

    Intermediary node

--- src/lib/components/svelteflow/Flow.svelte
+++ src/lib/components/svelteflow/Flow.svelte
@@ -1,111 +1,143 @@
 <script lang="ts">
-	import { writable } from 'svelte/store';
-	import {
-		SvelteFlow,
-		useSvelteFlow,
-		Background,
-		type Edge,
-		type Node,
-		type OnConnectEnd
-	} from '@xyflow/svelte';
-	import '@xyflow/svelte/dist/style.css';
-	import { initialNodes, initialEdges } from './nodes-and-edges';
-	import CustomNode from './CustomNode.svelte';
+    import { writable } from 'svelte/store'
+    import {
+        SvelteFlow,
+        useSvelteFlow,
+        Background,
+        type Edge,
+        type Node,
+        type OnConnectEnd,
+    } from '@xyflow/svelte'
+    import '@xyflow/svelte/dist/style.css'
+    import { initialNodes, initialEdges } from './nodes-and-edges'
+    import CustomNode from './nodes/CustomNode.svelte'
+    import IntermediaryNode from './nodes/IntermediaryNode.svelte'
 
-	const nodes = writable<Node[]>(initialNodes);
-	const edges = writable<Edge[]>(initialEdges);
+    const nodes = writable<Node[]>(initialNodes)
+    const edges = writable<Edge[]>(initialEdges)
 
-	let rect: DOMRectReadOnly;
-	let id = initialEdges.length + 2;
-	const getId = () => `${id++}`;
+    let rect: DOMRectReadOnly
+    let id = initialEdges.length + 2
+    const getId = () => `${id++}`
 
-	const { screenToFlowPosition } = useSvelteFlow();
+    const { screenToFlowPosition } = useSvelteFlow()
 
-	const handleConnectEnd: OnConnectEnd = (event, connectionState) => {
-		if (connectionState.isValid) return;
+    let isIntermediaryMenuOpenned = $state(false)
+    const nodeTypes = {
+        custom: CustomNode,
+        intermediary: IntermediaryNode,
+    }
 
-		const sourceNodeId = connectionState.fromNode?.id ?? '1';
-		const id = getId();
-		const { clientX, clientY } = 'changedTouches' in event ? event.changedTouches[0] : event;
+    const addEdge = (newEdge: Edge) => {
+        edges.update((e) => [...e, newEdge])
+    }
 
-		const newNode: Node = {
-			id,
-			type: 'custom', // Use custom node type
-			data: {
-				name: `Node ${id}`,
-				job: 'New Node',
-				emoji: '🔄'
-			},
-			position: screenToFlowPosition({
-				x: clientX,
-				y: clientY
-			}),
-			origin: [0.5, 0.0]
-		};
+    function deleteNode(node: Node) {
+        nodes.update((n) => n.filter((n) => n.id !== node.id))
+    }
 
-		$nodes.push(newNode);
-		$edges.push({
-			source: sourceNodeId,
-			target: id,
-			id: `${sourceNodeId}--${id}`
-		});
+    const handleConnectEnd: OnConnectEnd = (event, connectionState) => {
+        if (connectionState.isValid) return
 
-		$nodes = $nodes;
-		$edges = $edges;
-	};
+        const sourceNodeId = connectionState.fromNode?.id ?? '1'
+        const id = getId()
+        const { clientX, clientY } =
+            'changedTouches' in event ? event.changedTouches[0] : event
 
-	const nodeTypes = {
-		custom: CustomNode
-	};
+        const newNode: Node = {
+            id,
+            type: 'intermediary',
+            data: {
+                name: `Node ${id}`,
+                job: 'New Node',
+                emoji: '🔄',
+            },
+            position: screenToFlowPosition({
+                x: clientX,
+                y: clientY,
+            }),
+            origin: [0.5, 0.0],
+        }
+
+        nodes.update((n) => [...n, newNode])
+        edges.update((e) => [
+            ...e,
+            {
+                source: sourceNodeId,
+                target: id,
+                id: `${sourceNodeId}--${id}`,
+            },
+        ])
+
+        isIntermediaryMenuOpenned = true
+
+        $nodes = $nodes
+        $edges = $edges
+    }
+    function handlePaneClick(event: MouseEvent | TouchEvent) {
+        if (isIntermediaryMenuOpenned) {
+            const intermediaryNodeToDelete = $nodes.find(
+                (node) => node.type === 'intermediary',
+            )
+            if (intermediaryNodeToDelete) {
+                deleteNode(intermediaryNodeToDelete)
+            }
+            isIntermediaryMenuOpenned = false
+        }
+    }
 </script>
 
 <svelte:window />
 
 <div class="wrapper" bind:contentRect={rect}>
-	<SvelteFlow
-		{nodes}
-		{nodeTypes}
-		{edges}
-		fitView
-		fitViewOptions={{ padding: 2 }}
-		onconnectend={handleConnectEnd}
-	>
-		<Background />
-	</SvelteFlow>
+    <SvelteFlow
+        {nodes}
+        {nodeTypes}
+        {edges}
+        fitView
+        fitViewOptions={{ padding: 2 }}
+        onconnectend={handleConnectEnd}
+        on:paneclick={(e) => handlePaneClick(e.detail.event)}
+    >
+        <Background />
+    </SvelteFlow>
 </div>
 
 <style>
-	:global(.svelte-flow .svelte-flow__handle) {
-		width: 30px;
-		height: 14px;
-		border-radius: 3px;
-		background-color: #784be8;
-	}
+    :global(.svelte-flow .svelte-flow__handle) {
+        width: 30px;
+        height: 14px;
+        border-radius: 3px;
+        background-color: #784be8;
+    }
 
-	:global(.svelte-flow .svelte-flow__handle-top) {
-		top: -10px;
-	}
+    :global(.svelte-flow .svelte-flow__handle-top) {
+        top: -10px;
+    }
 
-	:global(.svelte-flow .svelte-flow__handle-bottom) {
-		bottom: -10px;
-	}
+    :global(.svelte-flow .svelte-flow__handle-bottom) {
+        bottom: -10px;
+    }
 
-	:global(.svelte-flow .svelte-flow__node) {
-		height: 40px;
-		width: 150px;
-		justify-content: center;
-		align-items: center;
-		display: flex;
-		border-width: 2px;
-		font-weight: 700;
-	}
+    :global(.svelte-flow .svelte-flow__node) {
+        height: 40px;
+        width: 150px;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        border-width: 2px;
+        font-weight: 700;
+    }
 
-	:global(.svelte-flow .svelte-flow__edge path, .svelte-flow__connectionline path) {
-		stroke-width: 2;
-	}
+    :global(
+            .svelte-flow .svelte-flow__edge path,
+            .svelte-flow__connectionline path
+        ) {
+        stroke-width: 2;
+    }
 
-	.wrapper {
-		height: 100vh;
-		width: 100vw;
-	}
+    .wrapper {
+        height: 100vh;
+        width: 100vw;
+    }
 </style>
---
+++ src/lib/components/svelteflow/nodes/CustomNode.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import {
+        Handle,
+        Position,
+        type NodeProps,
+        NodeResizer,
+    } from '@xyflow/svelte'
+
+    let { data } = $props() as NodeProps
+</script>
+
+<div
+    class="relative rounded-md border-2 border-stone-400 bg-white px-4 py-2 shadow-md"
+>
+    <NodeResizer />
+
+    <div class="flex">
+        <div class="flex items-center justify-center rounded-full bg-gray-100">
+            {data.emoji}
+        </div>
+        <div class="ml-2">
+            <div class="text-lg font-bold">{data.name}</div>
+            <div class="text-gray-500">{data.job}</div>
+        </div>
+    </div>
+
+    <Handle
+        type="target"
+        position={Position.Left}
+        class="w-16 rounded-none border-none !bg-teal-500"
+    />
+    <Handle
+        type="source"
+        position={Position.Right}
+        class="w-16 rounded-none border-none !bg-teal-500"
+    />
+</div>
