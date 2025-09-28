58908b5974f04557c91864e697fac65ee9e01518
dotNacer <nacer.ayed@outlook.fr>
2025-09-28 20:19:04 +0000

    feat: Add blur and fade transition effect to TestNode component
    - Implemented a new `blurAndFade` transition in utils for enhanced visual effects.
    - Updated `TestNode` component to utilize the `blurAndFade` transition on mount, improving user experience during node interactions.
    - Integrated Svelte's `fly` transition for additional animation on exit.
    
    This enhances the visual dynamics of node interactions within the application.

--- src/lib/components/svelteflow/nodes/TestNode.svelte
+++ src/lib/components/svelteflow/nodes/TestNode.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import { NodeResizer, type NodeProps, Handle, Position } from '@xyflow/svelte'
-	// Les infos sont déclarées ici, imaginons qu'on ai besoin d'un id ou autre
+	import { onMount } from 'svelte'
+	import { fly } from 'svelte/transition'
 	let { data, selected, width, height } = $props() as NodeProps
+	import { blurAndFade } from '$lib/utils'
+	let mounted = $state(false)
+
+	onMount(() => {
+		mounted = true
+	})
 </script>
 
-<NodeResizer minWidth={width} minHeight={height} isVisible={selected} />
-<div class="flex h-full w-full border bg-background p-2">{data.title}</div>
-<Handle type="target" position={Position.Left} />
-<Handle type="source" position={Position.Right} />
+{#if mounted}
+	<NodeResizer minWidth={width} minHeight={height} isVisible={selected} />
+	<div
+		in:blurAndFade={{ y: -10, duration: 75 }}
+		out:fly={{ y: 100, duration: 150 }}
+		class="flex h-full w-full border bg-background p-2"
+	>
+		{data.title}
+	</div>
+	<Handle type="target" position={Position.Left} />
+	<Handle type="source" position={Position.Right} />
+{/if}
